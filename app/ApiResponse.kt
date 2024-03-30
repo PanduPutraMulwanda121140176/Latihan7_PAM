0c7ea1b675f0c293d57cb3dc17a3025e049cb2b9
Pandu <putrapandu530@gmail.com>
2024-03-30 12:24:08 +0700

    Initial commit

---
+++ app/ApiResponse.kt
@@ -0,0 +1,3 @@
+data class ApiResponse(
+    val data: List<User>
+)
---
+++ app/src/main/res/User.kt
@@ -0,0 +1,7 @@
+data class User(
+    val id: Int,
+    val email: String,
+    val firstName: String,
+    val lastName: String,
+    val avatar: String
+)
